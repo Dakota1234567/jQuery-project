f22ebcb293904833ccb1e8e511e28aaf69ab4f3d
Jenna <micheau.jenna@gmail.com>
2019-02-13 09:43:49 +0100

    nav fixed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $(window).on("scroll",function(){
     let positionY = $(this).scrollTop();
-    console.log(positionY);
-    if (positionY > 1500){
-        $("#scrolltop").css("opacity", 1);
+
+    // Afficher arrototop une fois en bas de la page
+    if(positionY > 1500){
+        $("#scrolltotop").css("opacity", 1);
     }
-    if (positionY < 1500){
-        $("#scrolltop").css("opacity", 0);
+    if(positionY < 1500){
+        $("#scrolltotop").css("opacity", 0);
     }
+
+    // lorsque positionY est supérieur à 100
+    // réduire height de la nav 
+    if(positionY > 100){
+        $("ul").css("height", 50);
+        $("ul").css("position", "fixed");
+    }
+    // positionner la nav en fixe en haut de la page
 })
